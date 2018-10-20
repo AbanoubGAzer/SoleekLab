4e84893a44f2794b69cc84c9c8a4e353c599d8c6
Abanoub <abanoub@Abanoubs-Mac.local>
2018-10-20 06:33:28 -0700

    Initial Commit

---
+++ SoleekLabApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoleekLabApplication
+//
+//  Created by Abanoub on 10/20/18.
+//  Copyright © 2018 Abanoub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
